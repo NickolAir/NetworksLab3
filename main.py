b8b776a0b737653e4dcffb2532719c1a685cfe41
Nikolay Ratushnyak <112893665+NickolAir@users.noreply.github.com>
2023-10-28 13:50:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,26 @@ API_KEY_LOCATIONS = open('api_key_locations', 'r').read()
 
 CITY = input()
 
+url1 = BASE_URL1 + "?key=" + API_KEY_LOCATIONS + "&q=" + CITY + "&limit=" + "10"
+
+# Выполняем запрос
+response = requests.get(url1)
+
+# Проверяем успешность запроса
+if response.status_code == 200:
+    data = response.json()
+    if 'hits' in data:
+        for hit in data['hits']:
+            print(f'Место: {hit["name"]}, Координаты: ({hit["point"]["lat"]}, {hit["point"]["lng"]})')
+    else:
+        print('Результаты не найдены.')
+else:
+    print(f'Ошибка при выполнении запроса: {response.status_code}')
+
 def KelvinToCelsius(kelvin):
     celsius = kelvin - 273.15
     fahrenheit = celsius * (9 / 5) + 32
     return celsius, fahrenheit
-
-url1 = BASE_URL1 + "?key=" + API_KEY_LOCATIONS + "&q=" + CITY
-response = requests.get(url1).json()
-print(response)
 
 url2 = BASE_URL2 + "appid=" + API_KEY_WEATHER + "&q=" + CITY
 response = requests.get(url2).json()
@@ -31,6 +43,7 @@ description = response['weather'][0]['description']
 sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
 sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
 
+print()
 print(f"Temperature in {CITY}: {temp_celsius:.2f}°C or {temp_fahrenheit}°F")
 print(f"Temperature in {CITY} feels like: {feels_like_celsius:.2f}°C or {feels_like_fahrenheit:.2f}F")
 print(f"Humidity in {CITY}: {humidity}%")
